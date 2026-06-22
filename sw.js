1dce5772dfc97500cf3d2d739db7e418106aefca
Jardeson0607 <jardesonofc@gmail.com>
2026-06-22 00:32:08 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'kauekids-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Instalar o Service Worker e guardar a estrutura básica no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Ativar e limpar caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Responder requisições buscando no cache primeiro ou indo na rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
